b02bc6270302a459e4608db6651451c80446dc2b
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-19 15:32:36 -0800

    function that quits with ctrl + c

--- play.js
+++ play.js
@@ -1,7 +1,18 @@
 const { connect } = require('./client');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = ('data', () => {
+  if (key === '\u0003') {
+    process.exit
+  }
+})
 
-
-
-
+setupInput();
