2a8f01d81554b22d5b6f3630ee1835e9935da128
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-19 15:40:43 -0800

    imported functions from input.js

--- play.js
+++ play.js
@@ -1,18 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = ('data', () => {
-  if (key === '\u0003') {
-    process.exit
-  }
-})
+console.log("Connecting ...");
+connect();
 
 setupInput();
