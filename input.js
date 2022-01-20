753598dc2f35a28f61d6068f6a5ba8b1443d50c5
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-19 16:52:01 -0800

    implemented 4 directional movement

--- input.js
+++ input.js
@@ -1,18 +1,38 @@
-const setupInput = function() {
+const { connect } = require("./play");
+
+let connection;
+
+
+// setup interface to handle user input from stdin
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', handleUserInput);
   return stdin;
 };
 
-const handleUserInput = ('data', () => {
-  if (key === '\u0003') {
-    process.exit
-  }
-})
 
-setupInput();
+const handleUserInput = function(key) {
+  console.log(key)
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up')
+  }
+  if (key === 's') {
+    connection.write('Move: down')
+  }
+  if (key === 'a') {
+    connection.write('Move: left')
+  }
+  if (key === 'd') {
+    connection.write('Move: right')
+  }
+
+}
 
 module.exports = { setupInput }
