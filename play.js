05dd9c5a72579f788eb83b50f50fcc9d97fccf8b
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-19 16:51:09 -0800

    establish connection code

--- play.js
+++ play.js
@@ -2,6 +2,6 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+const con = connect()
+setupInput(con);
